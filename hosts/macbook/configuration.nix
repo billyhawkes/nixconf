bce4a93812500d260b8113385bf7371004670670
Billy Hawkes <billyhawkes02@gmail.com>
2026-05-20 19:15:54 -0400

    task: add aws config

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -17,9 +17,9 @@ in
   };
 
   system.activationScripts.postActivation.text = ''
-    install -d -m 0755 -o ${user} -g staff ${home}/.config/ghostty
-    ln -sfn /etc/ghostty/config ${home}/.config/ghostty/config
-    chown -h ${user}:staff ${home}/.config/ghostty/config
+    install -d -m 0755 -o ${user} -g staff ${home}/.aws
+    ln -sfn /etc/aws/config ${home}/.aws/config
+    chown -h ${user}:staff ${home}/.aws/config
   '';
 
   system.stateVersion = 6;
@@ -28,5 +28,18 @@ in
     [user]
       email = "billyhawkes02@gmail.com"
       name = "Billy Hawkes"
+  '';
+
+  environment.etc."aws/config".text = ''
+    [profile bhawkes@krakconsultants.com]
+    sso_session = bhawkes@krakconsultants.com
+    sso_account_id = 699475939168
+    sso_role_name = AdministratorAccess
+    region = ca-central-1
+
+    [sso-session bhawkes@krakconsultants.com]
+    sso_start_url = https://ssoins-8824190e0c2cdd70.portal.ca-central-1.app.aws
+    sso_region = ca-central-1
+    sso_registration_scopes = sso:account:access
   '';
 }
